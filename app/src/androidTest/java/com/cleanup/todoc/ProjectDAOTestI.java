ff40dbc0ee1fc95ab9bde28ca3fb273ff57a8d08
Mickael <mickael.jean.20@gmail.com>
2022-09-03 17:47:57 +0200

    "Intégration app release apk"

---
+++ app/src/androidTest/java/com/cleanup/todoc/ProjectDAOTestI.java
@@ -0,0 +1,12 @@
+package com.cleanup.todoc;
+
+import org.junit.runner.RunWith;
+
+import androidx.test.runner.AndroidJUnit4;
+
+@RunWith(AndroidJUnit4.class)
+public class ProjectDAOTestI {
+
+
+
+}
